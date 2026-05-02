b125ca9f218eb1520a75171b34d8cb322ae9fb26
joschonarth <joschonarth@gmail.com>
2026-05-02 17:26:57 -0300

    feat: implement findAllByMuscleGroup method using JPQL and native query

--- src/main/java/br/com/joschonarth/spring_boot_essentials/database/repository/IExerciseRepository.java
+++ src/main/java/br/com/joschonarth/spring_boot_essentials/database/repository/IExerciseRepository.java
@@ -2,6 +2,9 @@ package br.com.joschonarth.spring_boot_essentials.database.repository;
 
 import br.com.joschonarth.spring_boot_essentials.database.model.ExerciseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.NativeQuery;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
@@ -9,4 +12,18 @@ import java.util.UUID;
 public interface IExerciseRepository extends JpaRepository<ExerciseEntity, UUID> {
 
     List<ExerciseEntity> findAllByMuscleGroup(String muscleGroup);
+
+    @Query(value = """
+        SELECT e
+        FROM ExerciseEntity e
+        WHERE UPPER(e.muscleGroup) = UPPER(:muscleGroup)
+    """)
+    List<ExerciseEntity> findAllByMuscleGroupJpql(@Param("muscleGroup") String muscleGroup);
+
+    @NativeQuery(value = """
+        SELECT e
+        FROM exercise e
+        WHERE UPPER(e.muscle_group) = UPPER(:muscleGroup)
+    """)
+    List<ExerciseEntity> findAllByMuscleGroupNative(@Param("muscleGroup") String muscleGroup);
 }
